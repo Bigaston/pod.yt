56d4f78cbd1247c5fd30715ed27865520183425b
Jules <bigaston1@gmail.com>
2020-10-20 21:50:41 +0200

    Ajout du format youtube /user

--- modules/main_ctrl.js
+++ modules/main_ctrl.js
@@ -8,7 +8,7 @@ const jwt = require("jsonwebtoken")
 const fs = require("fs");
 const mustache = require("mustache");
 
-let yt_regex = new RegExp(/(https:\/\/www\.youtube\.com\/channel\/[A-Za-z0-9]+)|(https:\/\/www\.youtube\.com\/playlist\?list=[A-Za-z0-9]+)|(https:\/\/www\.youtube\.com\/c\/[A-Za-z0-9]+)/);
+let yt_regex = new RegExp(/(https:\/\/www\.youtube\.com\/channel\/[A-Za-z0-9]+)|(https:\/\/www\.youtube\.com\/playlist\?list=[A-Za-z0-9]+)|(https:\/\/www\.youtube\.com\/c\/[A-Za-z0-9]+)|(https:\/\/www\.youtube\.com\/user\/[A-Za-z0-9]+)/);
 let code_regex = new RegExp(/[A-Za-z][A-Za-z0-9]{1,24}/);
 let email_regex = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)
 
