e1988a107761fd825f780b89c926f20ac217a24f
JulesP <poulain@c-koya.tech>
2021-08-31 12:45:40 +0200

    Ajout du support de podcloud

--- modules/redirect_ctrl.js
+++ modules/redirect_ctrl.js
@@ -1,11 +1,49 @@
 const bdd = require("./bdd").get();
+const https = require("https");
 
 module.exports = {
-    redirect: (req, res) => {
-        if (bdd.link[req.params.code] != undefined) {
-            res.redirect(bdd.link[req.params.code].to);
-        } else {
-            res.redirect("/~404");
+  redirect: (req, res) => {
+    if (req.params.code.startsWith("@")) {
+      let query = `{
+				podcast(identifier:"${req.params.code.replace("@", "").toLowerCase()}") {
+					socials {
+						youtube
+					}
+				}
+			}`;
+
+      let request = https.request(
+        `https://feeds.podcloud.fr/graphql?query=${encodeURI(query)}`,
+        (response) => {
+          response.on("data", (d) => {
+            let result = JSON.parse(d);
+
+            if (result.data.podcast === null) {
+              res.redirect("/~404");
+            } else {
+              if (result.data.podcast.socials.youtube === null) {
+                res.redirect("/~404");
+              } else {
+                res.redirect(
+                  "https://youtube.com/" + result.data.podcast.socials.youtube
+                );
+              }
+            }
+          });
         }
+      );
+
+      request.on("error", (error) => {
+        res.redirect("/~404");
+      });
+
+      request.end();
+    } else {
+      if (bdd.link[req.params.code] != undefined) {
+        res.redirect(bdd.link[req.params.code].to);
+      } else {
+        res.redirect("/~404");
+      }
     }
-}
+  },
+};
--- podyt.js
+++ podyt.js
@@ -1,25 +1,26 @@
 require("dotenv").config();
 
-const express = require('express')
-const m = require("./modules")
-const bodyParser = require('body-parser')
-const session = require('express-session')
+const express = require("express");
+const m = require("./modules");
+const session = require("express-session");
 
-m.link.flush_link()
+m.link.flush_link();
 setInterval(m.link.flush_link, 1000 * 60 * 5);
 
-var app = express()
+var app = express();
 
-app.use(bodyParser.urlencoded({ extended: true })) 
-app.use(bodyParser.json());
-app.use("/public", express.static('./web/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/public", express.static("./web/public"));
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }
-}))
+    cookie: { secure: false },
+  })
+);
 
 app.get("/", m.main_ctrl.index);
 app.post("/~add_link", m.main_ctrl.add_link);
@@ -32,11 +33,25 @@ app.get("/~404", m.main_ctrl.not_found);
 app.get("/~login", m.admin_ctrl.login_form);
 app.post("/~login", m.admin_ctrl.admin_post);
 app.get("/~a/dashboard", m.admin_ctrl.is_logged, m.admin_ctrl.dashboard);
-app.get("/~a/dashboard/all", m.admin_ctrl.is_logged, m.admin_ctrl.dashboard_all)
-app.get("/~a/accept/:code", m.admin_ctrl.is_logged_request, m.admin_ctrl.accept);
-app.get("/~a/reject/:code/:raison", m.admin_ctrl.is_logged_request, m.admin_ctrl.reject);
+app.get(
+  "/~a/dashboard/all",
+  m.admin_ctrl.is_logged,
+  m.admin_ctrl.dashboard_all
+);
+app.get(
+  "/~a/accept/:code",
+  m.admin_ctrl.is_logged_request,
+  m.admin_ctrl.accept
+);
+app.get(
+  "/~a/reject/:code/:raison",
+  m.admin_ctrl.is_logged_request,
+  m.admin_ctrl.reject
+);
 
 // Route de redirection
-app.get("/:code", m.redirect_ctrl.redirect)
+app.get("/:code", m.redirect_ctrl.redirect);
 
-app.listen(process.env.PORT, () => console.log(`Serveur lancé sur ${process.env.PORT}`))
+app.listen(process.env.PORT, () =>
+  console.log(`Serveur lancé sur ${process.env.PORT}`)
+);
